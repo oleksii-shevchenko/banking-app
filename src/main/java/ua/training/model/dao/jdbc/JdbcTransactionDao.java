14d715e4c09ab581a5825e4d9e2343c9da144ee9
AlexShevchenko <alex.shevchenko.ua@gmail.com>
2018-12-08 00:56:03 +0200

    Finish main part of TransactionDao

--- src/main/java/ua/training/model/dao/jdbc/JdbcTransactionDao.java
+++ src/main/java/ua/training/model/dao/jdbc/JdbcTransactionDao.java
@@ -1,38 +1,98 @@
 package ua.training.model.dao.jdbc;
 
 import ua.training.model.dao.TransactionDao;
+import ua.training.model.dao.mapper.Mapper;
+import ua.training.model.dao.mapper.factory.JdbcMapperFactory;
 import ua.training.model.entity.Transaction;
+import ua.training.model.exception.UnsupportedOperationException;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcTransactionDao implements TransactionDao {
     @Override
     public List<Transaction> getAllTransactionsForAccount(Long accountId) {
-        return null;
+        try (Connection connection = ConnectionsPool.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(QueriesManager.getQuery("sql.transactions.get.by.account"))) {
+            preparedStatement.setLong(1, accountId);
+            preparedStatement.setLong(2, accountId);
+            return createListFromResultSet(preparedStatement.executeQuery());
+        } catch (SQLException exception) {
+            //todo add logger
+            throw new RuntimeException(exception);
+        }
     }
 
     @Override
     public Transaction get(Long key) {
-        return null;
+        try (Connection connection = ConnectionsPool.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(QueriesManager.getQuery("sql.transactions.get.by.id"))) {
+            preparedStatement.setLong(1, key);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                return new JdbcMapperFactory().getTransactionMapper().map(resultSet);
+            } else {
+                throw new SQLException();
+            }
+        } catch (SQLException exception) {
+            //todo add logger
+            throw new RuntimeException(exception);
+        }
     }
 
     @Override
     public List<Transaction> get(List<Long> keys) {
-        return null;
+        try (Connection connection = ConnectionsPool.getConnection()) {
+            connection.setAutoCommit(false);
+            try (PreparedStatement preparedStatement = connection.prepareStatement(QueriesManager.getQuery("sql.transactions.get.by.id"))) {
+                for (Long key : keys) {
+                    preparedStatement.setLong(1, key);
+                    preparedStatement.addBatch();
+                }
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+                connection.commit();
+
+                return createListFromResultSet(resultSet);
+            }
+        } catch (SQLException exception) {
+            //todo add logger
+            throw new RuntimeException(exception);
+        }
     }
 
+    //todo add comment that insert possible only in transact operations
     @Override
     public Long insert(Transaction entity) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
+    //todo add comment that transaction records is immutable in db
     @Override
     public void update(Transaction entity) {
-
+        throw new UnsupportedOperationException();
     }
 
+    //todo add comment that transaction records is immutable in db
     @Override
     public void remove(Transaction entity) {
+        throw new UnsupportedOperationException();
+    }
 
+    private List<Transaction> createListFromResultSet(ResultSet resultSet) throws SQLException {
+        Mapper<Transaction, ResultSet> mapper = new JdbcMapperFactory().getTransactionMapper();
+
+        List<Transaction> transactions = new ArrayList<>();
+        while (resultSet.next()) {
+            transactions.add(mapper.map(resultSet));
+        }
+
+        return transactions;
     }
 }
