f5e5118cc23ef98893cf2ab6771c798b5201dff3
AlexShevchenko <alex.shevchenko.ua@gmail.com>
2018-12-10 14:55:58 +0200

    Add result set mapper

--- src/main/java/ua/training/model/dao/mapper/jdbc/JdbcUserMapper.java
+++ src/main/java/ua/training/model/dao/mapper/jdbc/JdbcUserMapper.java
@@ -4,10 +4,23 @@ import ua.training.model.dao.mapper.Mapper;
 import ua.training.model.entity.User;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class JdbcUserMapper implements Mapper<User, ResultSet> {
     @Override
     public User map(ResultSet resultSet) {
-        return null;
+        try {
+            return User.getBuilder()
+                    .setId(resultSet.getLong("user_id"))
+                    .setLogin(resultSet.getString("user_login"))
+                    .setPasswordHash(resultSet.getString("password_hash"))
+                    .setEmail(resultSet.getString("user_email"))
+                    .setRole(User.Role.valueOf(resultSet.getString("user_role")))
+                    .setFirstName(resultSet.getString("first_name"))
+                    .setSecondName(resultSet.getString("second_name"))
+                    .build();
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 }
