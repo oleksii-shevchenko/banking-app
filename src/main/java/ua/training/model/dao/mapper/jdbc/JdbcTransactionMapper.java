388bdab69da0b5902b2ece10afe9379d055ae6c2
AlexShevchenko <alex.shevchenko.ua@gmail.com>
2018-12-10 15:00:36 +0200

    Add result set transaction mapper

--- src/main/java/ua/training/model/dao/mapper/jdbc/JdbcTransactionMapper.java
+++ src/main/java/ua/training/model/dao/mapper/jdbc/JdbcTransactionMapper.java
@@ -1,13 +1,27 @@
 package ua.training.model.dao.mapper.jdbc;
 
 import ua.training.model.dao.mapper.Mapper;
+import ua.training.model.entity.Currency;
 import ua.training.model.entity.Transaction;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class JdbcTransactionMapper implements Mapper<Transaction, ResultSet> {
     @Override
     public Transaction map(ResultSet resultSet) {
-        return null;
+        try {
+            return Transaction.getBuilder()
+                    .setId(resultSet.getLong("transaction_id"))
+                    .setTime(resultSet.getTimestamp("transaction_time").toLocalDateTime())
+                    .setSender(resultSet.getLong("sender"))
+                    .setReceiver(resultSet.getLong("receiver"))
+                    .setType(Transaction.Type.valueOf(resultSet.getString("transaction_type")))
+                    .setAmount(resultSet.getBigDecimal("transaction_amount"))
+                    .setCurrency(Currency.valueOf(resultSet.getString("transaction_currency")))
+                    .build();
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 }
