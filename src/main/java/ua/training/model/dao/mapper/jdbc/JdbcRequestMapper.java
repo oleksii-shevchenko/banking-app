2abcf7e653e60ff7f183be500a9fdada76e9fdea
AlexShevchenko <alex.shevchenko.ua@gmail.com>
2018-12-10 15:03:28 +0200

    Add result set request mapper

--- src/main/java/ua/training/model/dao/mapper/jdbc/JdbcRequestMapper.java
+++ src/main/java/ua/training/model/dao/mapper/jdbc/JdbcRequestMapper.java
@@ -1,13 +1,25 @@
 package ua.training.model.dao.mapper.jdbc;
 
 import ua.training.model.dao.mapper.Mapper;
+import ua.training.model.entity.Currency;
 import ua.training.model.entity.Request;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class JdbcRequestMapper implements Mapper<Request, ResultSet> {
     @Override
     public Request map(ResultSet resultSet) {
-        return null;
+        try {
+            return Request.getBuilder()
+                    .setId(resultSet.getLong("request_id"))
+                    .setRequesterId(resultSet.getLong("requester_id"))
+                    .setType(Request.Type.valueOf(resultSet.getString("type")))
+                    .setCurrency(Currency.valueOf(resultSet.getString("currency")))
+                    .setCompleted(resultSet.getBoolean("is_completed"))
+                    .build();
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 }
