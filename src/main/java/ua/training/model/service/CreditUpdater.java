87653a327309aaccac971ca4cd483d7fa8d3ae40
AlexShevchenko <alex.shevchenko.ua@gmail.com>
2018-12-11 16:22:18 +0200

    Add deposit updater

--- src/main/java/ua/training/model/service/CreditUpdater.java
+++ src/main/java/ua/training/model/service/CreditUpdater.java
@@ -10,12 +10,13 @@ import java.util.function.Function;
 
 /**
  * Realization of credit account updater. It modifies account according to credit policy.
+ * @see ScheduledUpdateService
  * @author Oleksii Shevchenko
  */
 public class CreditUpdater implements Function<Account, Transaction> {
     /**
      * Method realize credit policy. Account must be credit, if it isn't, then throws exception. If account has non
-     * negative balance then do no update then throws {@link TrivialUpdateException}).
+     * negative balance then do no update and throws {@link TrivialUpdateException}).
      * If account has negative account then accrues interest on the loan and creates transaction based on update.
      * @see ua.training.model.dao.TransactionDao#makeUpdate(Long, Function)
      * @see TrivialUpdateException
