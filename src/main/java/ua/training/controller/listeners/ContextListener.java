76fddae3e3c1a8204320094b05c7cef4c3783e67
AlexShevchenko <alex.shevchenko.ua@gmail.com>
2019-01-03 17:16:57 +0200

    Add info logger in context listener

--- src/main/java/ua/training/controller/listeners/ContextListener.java
+++ src/main/java/ua/training/controller/listeners/ContextListener.java
@@ -1,5 +1,7 @@
 package ua.training.controller.listeners;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ua.training.model.dao.factory.JdbcDaoFactory;
 import ua.training.model.service.CurrencyExchangeService;
 import ua.training.model.service.ScheduledTaskService;
@@ -10,10 +12,15 @@ import javax.servlet.annotation.WebListener;
 
 @WebListener
 public class ContextListener implements ServletContextListener {
+    private static Logger logger = LogManager.getLogger(ContextListener.class);
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         new ScheduledTaskService().init(JdbcDaoFactory.getInstance());
+        logger.info("Scheduled task service is online.");
+
         new CurrencyExchangeService().init();
+        logger.info("Currency exchange service is online.");
     }
 
     @Override
