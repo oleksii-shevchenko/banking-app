3d84d94cf0e4e239ed50c362bb8780c883998748
AlexShevchenko <alex.shevchenko.ua@gmail.com>
2019-03-02 15:04:00 +0200

    Rewrite info account command

--- src/main/java/ua/training/controller/commands/InfoAccountCommand.java
+++ src/main/java/ua/training/controller/commands/InfoAccountCommand.java
@@ -2,9 +2,9 @@ package ua.training.controller.commands;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import ua.training.controller.util.managers.PathManager;
-import ua.training.model.dao.factory.JdbcDaoFactory;
 import ua.training.model.entity.Account;
 import ua.training.model.entity.Currency;
 import ua.training.model.entity.User;
@@ -17,19 +17,24 @@ import javax.servlet.http.HttpServletRequest;
 public class InfoAccountCommand implements Command {
     private static Logger logger = LogManager.getLogger(InfoAccountCommand.class);
 
+    private AccountService accountService;
+    private UserService userService;
+
+    private PathManager pathManager;
+
     @Override
     public String execute(HttpServletRequest request) {
         Long accountId = Long.valueOf(request.getParameter("accountId"));
         Long userId = (Long) request.getSession().getAttribute("id");
         User.Role role = User.Role.valueOf((String) request.getSession().getAttribute("role"));
 
-        Account account = new AccountService(JdbcDaoFactory.getInstance()).getAccount(accountId);
-        User user = new UserService(JdbcDaoFactory.getInstance()).get(userId);
+        Account account = accountService.getAccount(accountId);
+        User user = userService.get(userId);
 
         if (!role.equals(User.Role.ADMIN) && !account.getHolders().contains(userId)) {
             logger.warn("User " + user.getId() + "try to access account " + accountId + " without permissions");
 
-            return "redirect:" + PathManager.getPath("path.error");
+            return "redirect:" + pathManager.getPath("path.error");
         }
 
         request.setAttribute("type", account.getClass().getSimpleName());
@@ -37,6 +42,21 @@ public class InfoAccountCommand implements Command {
         request.setAttribute("currencies", Currency.values());
         request.setAttribute("accountIds", user.getAccounts());
 
-        return PathManager.getPath("path.info.account");
+        return pathManager.getPath("path.info.account");
+    }
+
+    @Autowired
+    public void setAccountService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    public void setPathManager(PathManager pathManager) {
+        this.pathManager = pathManager;
     }
 }
