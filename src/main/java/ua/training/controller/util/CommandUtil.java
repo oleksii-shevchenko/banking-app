a91bdb4d304a31ae12aab5e615f6f7093a6a37d6
AlexShevchenko <alex.shevchenko.ua@gmail.com>
2019-03-02 15:52:04 +0200

    Rewrite controller package

--- src/main/java/ua/training/controller/util/CommandUtil.java
+++ src/main/java/ua/training/controller/util/CommandUtil.java
@@ -1,11 +1,14 @@
 package ua.training.controller.util;
 
+import org.springframework.stereotype.Component;
+
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * This util used for parsing command from request to app api.
  * @author Oleksii Shevchenko
  */
+@Component
 public class CommandUtil {
     /**
      * Method extracts command from request uri. Only request to api allowed in this method.
